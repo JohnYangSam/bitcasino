6b44379ebc93283c4d2e2467d7709c1da9c8ec6e
John YS <johnys@stanford.edu>
2014-05-17 19:16:33 -0700

    testing

--- routes/text.js
+++ routes/text.js
@@ -7,10 +7,7 @@ var utilities = require('../utilities');
 /* Redirect based on response . */
 router.post('/', function(req, res) {
   var body = req.param('Body').trim();
-  console.log("GOT HERE- top ---------------");
   console.log(body);
-  console.log("GOT HERE-- bottom --------------");
-  res.send(body);
 });
 
 
