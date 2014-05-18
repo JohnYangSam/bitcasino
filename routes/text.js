66306c9e8771538fbe5fbe9b2ff37a2c5feb486d
John YS <johnys@stanford.edu>
2014-05-17 21:54:04 -0700

    start switch

--- routes/text.js
+++ routes/text.js
@@ -3,12 +3,21 @@ var router = express.Router();
 var twilio = require('twilio');
 var utilities = require('../utilities');
 
+var helpMessage = "Actions:\n" +
+          "balance\n" +
+          "bet <# satoshi>\n" +
+          "roll\n" +
+          "widthraw <# satoshi>"+
+          "Send to XX address to deposit";
+
 /* Redirect based on response . */
 router.post('/', function(req, res) {
   // Validate request is from twilio
   if (twilio.validateExpressRequest(req, process.env.TWILIO_AUTH_TOKEN)) {
 
     console.log("--------Got here----------");
+
+    // Extract body parameters
     var body = req.param('Body').trim();
     var to = req.param('To').trim();
     var from = req.param('From').trim();
@@ -16,12 +25,19 @@ router.post('/', function(req, res) {
     var twiml = new twilio.TwimlResponse();
 
     switch(body) {
+      case 
+      case
+      case
+      case
+      case
+      case "help":
+        twiml.sms(helpMessage);
+        break;
       default:
-
-        console.log("default hit");
-        twiml.sms("this is a test");
+        twmil.sms("Unknown Command:\n" + helpMessage);
         break;
     }
+    
     console.log(twiml);
     console.log("--------sent back----------");
 
