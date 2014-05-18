2e754325ac01160f6454c2113b246de1809dc81b
John YS <johnys@stanford.edu>
2014-05-17 19:12:37 -0700

    add utilities

--- routes/text.js
+++ routes/text.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
 var client = new twilio.RestClient(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+var utilities = require('../utilities');
 
 /* Redirect based on response . */
 router.post('/', function(req, res) {
---
+++ utilities.js
@@ -0,0 +1,20 @@
+// Inspired by https://github.com/crabasa/votr-part1/blob/master/utils.js
+
+var utils = {};
+
+var MAX_SMS_LEN = 160;
+
+utils.toSMS = function (string) {
+  var result = "";
+  if (string.length < MAX_SMS_LEN) {
+    result = string;
+  } else {
+    result = substring(0, MAX_SMS_LEN - 3) + "...";
+  }
+  return result;
+}
+
+utils.isInt = function (string) {
+  var intRegex = /^\d+$/;
+  return intRegex.test(string);
+}
